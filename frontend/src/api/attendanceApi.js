4fe39d3ef89cb2eaaee18806be16e431b9a844b5
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-09-20 20:31:03 +0900

    react : api url 수정

--- frontend/src/api/attendanceApi.js
+++ frontend/src/api/attendanceApi.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { Cookies } from "react-cookie";
 
+
+// API URL을 환경에 따라 설정 (배포 환경과 개발 환경 구분)
+const isProd = process.env.NODE_ENV === "production"; // 배포 환경 감지
+const API_URL = isProd ? 'https://www.poko-dev.com/attendance/api/' : 'http://127.0.0.1:8000/attendance/api/';
+
+
 // CSRF 토큰을 가져오는 함수
 const getCSRFToken = () => {
     const cookies = new Cookies();
@@ -12,7 +18,7 @@ const csrftoken = getCSRFToken();
 // 출석 데이터를 가져오는 함수
 export const fetchAttendanceData = async (year) => {
     try {
-        const response = await axios.get('https://www.poko-dev.com/attendance/api/records', {
+        const response = await axios.get(`${API_URL}records/`, {
             params: { year }, // 파라미터 추가
             withCredentials: true, // 쿠키와 함께 요청
             headers: {
@@ -30,7 +36,7 @@ export const fetchAttendanceData = async (year) => {
 // 학생 데이터를 가져오는 함수
 export const fetchStudents = async () => {
     try {
-        const response = await axios.get('https://www.poko-dev.com/attendance/api/members', {
+        const response = await axios.get(`${API_URL}members/`, {
             withCredentials: true, // 쿠키와 함께 요청
             headers: {
                 'X-CSRFToken': csrftoken, // CSRF 토큰
--- frontend/src/pages/AttendancePage.js
+++ frontend/src/pages/AttendancePage.js
@@ -12,24 +12,24 @@ const  { Content } = Layout;
 const { Option } = Select; 
 
 // 임의의 출석 데이터 생성
-const mockAttendanceData = [
-  { date: '2023-06-30', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': true, '7': true, '8': false, '9': true, '10': false } },
-  { date: '2023-07-07', attendance: { '1': true, '2': true, '3': true, '4': false, '5': true, '6': false, '7': true, '8': true, '9': false, '10': true } },
-  { date: '2023-07-14', attendance: { '1': true, '2': false, '3': true, '4': true, '5': true, '6': true, '7': false, '8': true, '9': true, '10': false } },
-  { date: '2023-07-21', attendance: { '1': true, '2': true, '3': true, '4': true, '5': false, '6': false, '7': true, '8': true, '9': false, '10': true } },
-  { date: '2023-07-28', attendance: { '1': false, '2': true, '3': false, '4': true, '5': true, '6': true, '7': true, '8': false, '9': true, '10': true } },
-  { date: '2023-08-04', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': false, '7': true, '8': true, '9': false, '10': true } },
-  { date: '2023-08-11', attendance: { '1': true, '2': true, '3': true, '4': false, '5': true, '6': true, '7': true, '8': true, '9': false, '10': true } },
-  { date: '2023-08-18', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': true, '7': false, '8': true, '9': true, '10': true } }
-];
+// const mockAttendanceData = [
+//   { date: '2023-06-30', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': true, '7': true, '8': false, '9': true, '10': false } },
+//   { date: '2023-07-07', attendance: { '1': true, '2': true, '3': true, '4': false, '5': true, '6': false, '7': true, '8': true, '9': false, '10': true } },
+//   { date: '2023-07-14', attendance: { '1': true, '2': false, '3': true, '4': true, '5': true, '6': true, '7': false, '8': true, '9': true, '10': false } },
+//   { date: '2023-07-21', attendance: { '1': true, '2': true, '3': true, '4': true, '5': false, '6': false, '7': true, '8': true, '9': false, '10': true } },
+//   { date: '2023-07-28', attendance: { '1': false, '2': true, '3': false, '4': true, '5': true, '6': true, '7': true, '8': false, '9': true, '10': true } },
+//   { date: '2023-08-04', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': false, '7': true, '8': true, '9': false, '10': true } },
+//   { date: '2023-08-11', attendance: { '1': true, '2': true, '3': true, '4': false, '5': true, '6': true, '7': true, '8': true, '9': false, '10': true } },
+//   { date: '2023-08-18', attendance: { '1': true, '2': false, '3': true, '4': true, '5': false, '6': true, '7': false, '8': true, '9': true, '10': true } }
+// ];
 
   
 
 const AttendancePage = () => {
-     //const [attendanceData, setAttendanceData] = useState([]);
-    const [attendanceData, setAttendanceData] = useState(mockAttendanceData);
-    const [students, setStudents ] = useState([]);
+     const [attendanceData, setAttendanceData] = useState([]);
+    // const [attendanceData, setAttendanceData] = useState(mockAttendanceData);
     // const [students, setStudents] = useState(mockStudents); 
+    const [students, setStudents ] = useState([]);
     const [selectedYear, setSelectedYear ] = useState(new Date().getFullYear());
     const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 관리
     const [checkedStudents, setCheckedStudents] = useState([]);
