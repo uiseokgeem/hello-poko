8ea58eb7dc89a5f7c71a42eaf05c9f4330ea5357
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2025-02-08 22:40:17 +0900

    modify CustomUser for head_teacher field

--- poko/accounts/models.py
+++ poko/accounts/models.py
@@ -8,15 +8,41 @@ from .managers import UserManager
 # username = None: username 필드를 사용하지 않음을 명시.
 # USERNAME_FIELD = "email": 이메일을 사용자 이름 필드로 사용하여 인증.
 # REQUIRED_FIELDS = []: 이메일 외에 추가로 필수적인 필드가 없음을 설정
+
+
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
     full_name = models.CharField(max_length=4)
     birth_date = models.DateField(null=True, blank=True)
     registration_number = models.CharField(max_length=6, null=True, blank=True)
-    kakao_id = models.CharField(
-        max_length=100, unique=True, null=True, blank=True
-    )  # Kakao ID 필드 추가
+    kakao_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+
+    ROLE_CHOICES = (
+        ("HEAD", "정교사"),
+        ("ASSISTANT", "부교사"),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="ASSISTANT")
+
+    head_teacher = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="assistance_teacher",
+        # head_teacher = CustomUser.objects.get(email="head_teacher@example.com")
+        # assistants = head_teacher.assistant_teachers.all()  # 역참조로 소속된 부교사 조회
+        # for assistant in assistants:
+        #     print(assistant.full_name)  # 출력: 이부교1, 박부교2
+        limit_choices_to={"role": "HEAD"},
+        # 제한 조건: {"role": "HEAD"}
+        # role 필드의 값이 "HEAD"(정교사)인 객체만 선택 가능.
+        # 즉, role 필드가 "ASSISTANT"(부교사)인 객체는 선택할 수 없습니다.
+        # 부교사를 정교사로 설정하는 실수를 방지.
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
