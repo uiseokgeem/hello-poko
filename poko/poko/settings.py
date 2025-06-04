398e1e48b082c945df320d7c26973f36a97a11c6
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2025-06-04 19:27:33 +0900

    feat : ACCESS_TOKEN_LIFETIME 수정

--- poko/poko/settings.py
+++ poko/poko/settings.py
@@ -302,7 +302,7 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
